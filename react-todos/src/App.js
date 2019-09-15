6c0463f8a3dad287a8c345142f5b464078b63a53
Dylan Cruse <dylancruse@live.com>
2019-09-15 11:33:35 -0500

    Run create-react-app and initial file cleanup

---
+++ react-todos/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>App</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
