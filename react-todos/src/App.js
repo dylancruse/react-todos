a50e062387ad9d69d8cd8f382a7c82603555b467
Dylan Cruse <dylancruse@live.com>
2019-09-16 09:13:36 -0500

    Create Todos component and add to App.js

--- react-todos/src/App.js
+++ react-todos/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Todos from './components/Todos';
 import './App.css';
 
 class App extends React.Component {
     render() {
         return (
             <div className="App">
-                <h1>App</h1>
+                <Todos />
             </div>
         );
     }
---
+++ react-todos/src/components/Todos.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Todos extends React.Component {
+    render() {
+        return (
+            <div className="Todos">
+                <h1>Todos</h1>
+            </div>
+        );
+    }
+}
+
+export default Todos;
