49d5ad74073b9a1413cc2827cb23e4c4ccfb4a03
Dylan Cruse <dylancruse@live.com>
2019-09-16 14:04:21 -0500

    Code cleanup

--- react-todos/src/components/Todos.js
+++ react-todos/src/components/Todos.js
@@ -7,7 +7,8 @@ class Todos extends React.Component {
         return this.props.todos.map(
             (todo) => (
                 <TodoItem key={todo.id} todo={todo} toggleComplete={this.props.toggleComplete}
-                deleteTodo={this.props.deleteTodo} />
+                    deleteTodo={this.props.deleteTodo} 
+                />
             )
         );
     }
