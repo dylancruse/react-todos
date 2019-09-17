f084b7f4e5ea40238ee81b3ed230d39fb3831b9b
Dylan Cruse <dylancruse@live.com>
2019-09-16 19:37:17 -0500

    Updates

---
+++ precache-manifest.533e51a7d43dba168900b184d18c876b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "661d5ad071c34db28fdf2a3d32ec145b",
+    "url": "/react-todos/index.html"
+  },
+  {
+    "revision": "a9dfb014a56a9d019734",
+    "url": "/react-todos/static/css/main.01b2b9e6.chunk.css"
+  },
+  {
+    "revision": "7ca8d06f47507ac11062",
+    "url": "/react-todos/static/js/2.2e414e59.chunk.js"
+  },
+  {
+    "revision": "a9dfb014a56a9d019734",
+    "url": "/react-todos/static/js/main.69c84624.chunk.js"
+  },
+  {
+    "revision": "ef91f34a6a98c9d09d1a",
+    "url": "/react-todos/static/js/runtime~main.b2d33d76.js"
+  }
+]);
